971e5058c144355101dd4ab511e164021ac0cc2e
viroff <viroff_zz@mail.ru>
2023-05-18 21:38:18 +0300

    master form boneback

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function validatePassword(value) {
   if (!value) { return 'Password Required'; }
 }
 function validateYear(value) {
-  if (!value) {
+  if (!value || value === '-1') {
     return 'Укажите год выпуска';
   }
   else if (!/^(19|20)\d{2}$/i.test(value)) {
-    return "Неверно указан год выпуска";
+    return 'Неверно указан год выпуска';
   }
 }
 function validateMark(value) { console.log(value); }
@@ -32,16 +32,19 @@ function validateInfo(value) { }
 function validateCountry(value) { }
 function validateCity(value) { }
 function validatePhone(value) { }
+function validateGeneration(value) { }
+function validateModification(value) { }
+function validateConfiguration(value) { }
 
 function App() {
   return (
 
-    <div className="App">
+    <div className='App'>
       <Formik
         initialValues={{
-          Year: "",
-          email: "",
-          password: "",
+          Year: '',
+          email: '',
+          password: '',
         }}
         onSubmit={values => {
           console.log('submit', values);
@@ -49,117 +52,122 @@ function App() {
       >
         {({ errors, touched }) => (
           <Form>
-            <p className='link'>Автомобиль</p>
-            <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Year && touched.Year })}>
-              Год выпуска
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Year && touched.Year })}
-              name="Year"
-              validate={validateYear}
-            />
+            <p>--------Автомобиль---------</p>
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Year && touched.Year })}
+              name='Year'
+              validate={validateYear}>
+              <option selected value='-1'>Год выпуска</option>
+              <option value='2023'>2023</option>
+              <option value='2022'>2022</option>
+              <option value='2021'>2021</option>
+            </Field>
             {errors.Year && touched.Year && <div className={styles.error}>{errors.Year}</div>}
             <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Mark && touched.Mark })}>
-              Марка
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Mark && touched.Mark })}
-              name="Mark"
-              validate={validateMark}
-            />
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Mark && touched.Mark })}
+              name='Mark'
+              validate={validateMark}>
+              <option selected value='-1'>Марка</option>
+              <option value='1'>Audi</option>
+              <option value='2'>BMW</option>
+              <option value='3'>Mercedes-Benz</option>
+            </Field>
+            {errors.Mark && touched.Mark && <div className={styles.error}>{errors.Mark}</div>}
             <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Model && touched.Model })}>
-              Модель
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Model && touched.Model })}
-              name="Model"
-              validate={validateModel}
-            />
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Model && touched.Model })}
+              name='Model'
+              validate={validateModel}>
+              <option selected value='-1'>Модель</option>
+              <option value='1'>Ывыводллододл одл100</option>
+              <option value='2'>BMW</option>
+              <option value='3'>Mercedes-Benz</option>
+            </Field>
+            {errors.Model && touched.Model && <div className={styles.error}>{errors.Model}</div>}
             <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.State && touched.State })}>
-              Состояние
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.State && touched.State })}
-              name="State"
-              validate={validateState}
-            />
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Generation && touched.Generation })}
+              name='Model'
+              validate={validateGeneration}>
+              <option selected value='-1'>Поколение</option>
+              <option value='1'>Generation 1</option>
+              <option value='2'>Generation 2</option>
+              <option value='3'>Generation 3</option>
+            </Field>
+            {errors.Generation && touched.Generation && <div className={styles.error}>{errors.Generation}</div>}
             <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Mileage && touched.Mileage })}>
-              Пробег
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Mileage && touched.Mileage })}
-              name="Mileage"
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Configuration && touched.Configuration })}
+              name='Model'
+              validate={validateConfiguration}>
+              <option selected value='-1'>Конфигурация</option>
+              <option value='1'>Configuration 1</option>
+              <option value='2'>Configuration 2</option>
+              <option value='3'>Configuration 3</option>
+            </Field>
+            {errors.Configuration && touched.Configuration && <div className={styles.error}>{errors.Configuration}</div>}
+            <br />
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Modification && touched.Modification })}
+              name='Modification'
+              validate={validateModification}>
+              <option selected value='-1'>Модификация</option>
+              <option value='1'>Modification 1</option>
+              <option value='2'>Modification 2</option>
+              <option value='3'>Modification 3</option>
+            </Field>
+            {errors.Modification && touched.Modification && <div className={styles.error}>{errors.Modification}</div>}
+            <p>--------Подробности----------</p>
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.State && touched.State })}
+              name='State'
+              validate={validateState}>
+              <option selected value='-1'>Состояние</option>
+              <option value='1'>State 1</option>
+              <option value='2'>State 2</option>
+              <option value='3'>State 3</option>
+            </Field>
+            {errors.State && touched.State && <div className={styles.error}>{errors.State}</div>}
+            <br />
+            <Field placeholder='Пробег' className={classnames(styles.field, { [styles.errorInput]: errors.Mileage && touched.Mileage })}
+              name='Mileage'
               validate={validateMileage}
             />
             <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.SellingMethod && touched.SellingMethod })}>
-              Способ продажи
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.SellingMethod && touched.SellingMethod })}
-              name="SellingMethod"
-              validate={validateSellingMethod}
-            />
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.SellingMethod && touched.SellingMethod })}
+              name='SellingMethod'
+              validate={validateSellingMethod}>
+              <option selected value='-1'>Cпособ продажи</option>
+              <option value='1'>SellingMethod 1</option>
+              <option value='2'>SellingMethod 2</option>
+              <option value='3'>SellingMethod 3</option>
+            </Field>
+            {errors.SellingMethod && touched.SellingMethod && <div className={styles.error}>{errors.SellingMethod}</div>}
             <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Info && touched.Info })}>
-              Дополнительная информация
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Info && touched.Info })}
-              name="Info"
+            <Field type='textarea' placeholder='Описание' className={classnames(styles.field, { [styles.errorInput]: errors.Info && touched.Info })}
+              name='Info'
               validate={validateInfo}
             />
+            <p>--------Местонахождение---------</p>
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Country && touched.Country })}
+              name='Country'
+              validate={validateCountry}>
+              <option selected value='-1'>Страна</option>
+              <option value='1'>Country 1</option>
+              <option value='2'>Country 2</option>
+              <option value='3'>Country 3</option>
+            </Field>
+            {errors.Country && touched.Country && <div className={styles.error}>{errors.Country}</div>}
             <br />
-            Местонахождение
-            <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Country && touched.Country })}>
-              Страна
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Country && touched.Country })}
-              name="Country"
-              validate={validateCountry}
-            />
-            <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.City && touched.City })}>
-              Город
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.City && touched.City })}
-              name="City"
-              validate={validateCity}
-            />
-            <br />
-            Контакты
-            <br />
-            <label className={classnames(styles.label, { [styles.errorLabel]: errors.Phone && touched.Phone })}>
-              Телефон
-            </label>
-            <Field className={classnames(styles.field, { [styles.errorInput]: errors.Phone && touched.Phone })}
-              name="Phone"
+            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.City && touched.City })}
+              name='City'
+              validate={validateCity}>
+              <option selected value='-1'>Город</option>
+              <option value='1'>City 1</option>
+              <option value='2'>City 2</option>
+              <option value='3'>City 3</option>
+            </Field>
+            {errors.City && touched.City && <div className={styles.error}>{errors.City}</div>}
+            <p>--------Контакты---------</p>
+            <Field type='text' placeholder='Телефон' className={classnames(styles.field, { [styles.errorInput]: errors.Phone && touched.Phone })}
+              name='Phone'
               validate={validatePhone}
             />
-            {errors.Year && touched.Year ||
-              errors.Mark && touched.Mark ||
-              errors.Model && touched.Model ||
-              errors.State && touched.State ||
-              errors.Mileage && touched.Mileage ||
-              errors.SellingMethod && touched.SellingMethod ||
-              errors.Info && touched.Info ||
-              errors.Country && touched.Country ||
-              errors.City && touched.City ||
-              errors.Phone && touched.Phone(
-                <div className={styles.error}>
-                  {errors.Year}
-                  {errors.Mark}
-                  {errors.Model}
-                  {errors.State}
-                  {errors.Mileage}
-                  {errors.SellingMethod}
-                  {errors.Info}
-                  {errors.Country}
-                  {errors.City}
-                  {errors.Phone}
-                </div>
-              )}
             <br />
-
             <button type='submit'>Submit</button>
           </Form>
         )}
@@ -168,3 +176,21 @@ function App() {
     </div>)
 }
 export default App;
+
+
+/*
+<br />
+<label className={classnames(styles.label, { [styles.errorLabel]: errors.Year && touched.Year })}>
+  Год выпуска
+</label>
+<Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Year && touched.Year })}
+  name='Year'
+  validate={validateYear}>
+  <option selected value='-1'>Выберите год выпуска</option>
+  <option value='2023'>2023</option>
+  <option value='2022'>2022</option>
+  <option value='2021'>2021</option>
+</Field>
+{errors.Year && touched.Year && <div className={styles.error}>{errors.Year}</div>}
+<br />
+*/
