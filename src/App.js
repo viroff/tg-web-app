fe73346230e0fe9a83a31b8a30b2831e092aa6eb
viroff <viroff_zz@mail.ru>
2023-05-26 13:01:52 +0300

    previous markup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,42 @@ import classnames from 'classnames';
 import { Formik, Form, Field } from 'formik';
 import styles from './App.css';
 const tg = window.Telegram.WebApp;
+const carSelectionSteps =
+  [
+    {
+      'name': 'Year',
+    },
+    {
+      'name': 'Mark',
+    },
+    {
+      'name': 'Model',
+    },
+    {
+      'name': 'Generation',
+    },
+    {
+      'name': 'Configuration',
+    },
+    {
+      'name': 'Modification',
+    },
+  ];
+function stepOn(stepNumber) {
+  if (stepNumber >= carSelectionSteps.length) {
+    console.log('next stepper');
+  }
+  else {
+    const nextStepInputId = carSelectionSteps[stepNumber].name;
+    document.getElementsByName(nextStepInputId)[0].removeAttribute('disabled');
+    for (let i = stepNumber + 1; i < carSelectionSteps.length; i++) {
+      let inputId = carSelectionSteps[i].name;
+      document.getElementsByName(inputId)[0].setAttribute('disabled', '');
+      document.getElementsByName(inputId)[0].value='';
+    }
+  }
+}
+
 function validateEmail(value) {
   if (!value) {
     return 'Email Required';
@@ -15,16 +51,62 @@ function validateEmail(value) {
 function validatePassword(value) {
   if (!value) { return 'Password Required'; }
 }
+
 function validateYear(value) {
-  if (!value || value === '-1') {
+  if (!value || value === '') {
     return 'Укажите год выпуска';
   }
   else if (!/^(19|20)\d{2}$/i.test(value)) {
     return 'Неверно указан год выпуска';
   }
+  stepOn(1);
 }
-function validateMark(value) { console.log(value); }
-function validateModel(value) { console.log(value); }
+
+function validateMark(value) {
+  if (!value || value === '') {
+    return 'Укажите марку';
+  }
+  else if (!/^[^\s@]+$/i.test(value)) {
+    return 'Неверно указана марка';
+  }
+  stepOn(2);
+}
+function validateModel(value) {
+  if (!value || value === '') {
+    return 'Укажите модель';
+  }
+  else if (!/^[^\s@]+$/i.test(value)) {
+    return 'Неверно указана модель';
+  }
+  stepOn(3);
+}
+function validateGeneration(value) {
+  if (!value || value === '') {
+    return 'Укажите поколение';
+  }
+  else if (!/^[^\s@]+$/i.test(value)) {
+    return 'Неверно указано поколение';
+  }
+  stepOn(4);
+}
+function validateConfiguration(value) {
+  if (!value || value === '') {
+    return 'Укажите конфигурацию';
+  }
+  else if (!/^[^\s@]+$/i.test(value)) {
+    return 'Неверно указана конфигурация';
+  }
+  stepOn(5);
+}
+function validateModification(value) {
+  if (!value || value === '') {
+    return 'Укажите модификацию';
+  }
+  else if (!/^[^\s@]+$/i.test(value)) {
+    return 'Неверно указана модификация';
+  }
+  stepOn(6);
+}
 function validateState(value) { }
 function validateMileage(value) { }
 function validateSellingMethod(value) { }
@@ -32,9 +114,6 @@ function validateInfo(value) { }
 function validateCountry(value) { }
 function validateCity(value) { }
 function validatePhone(value) { }
-function validateGeneration(value) { }
-function validateModification(value) { }
-function validateConfiguration(value) { }
 
 function App() {
   return (
@@ -43,8 +122,18 @@ function App() {
       <Formik
         initialValues={{
           Year: '',
-          email: '',
-          password: '',
+          Mark: '',
+          Model: '',
+          Generation: '',
+          Configuration: '',
+          Modification: '',
+          State: '',
+          Mileage: '',
+          SellingMethod: '',
+          Info: '',
+          Country: '',
+          City: '',
+          Phone: '',
         }}
         onSubmit={values => {
           console.log('submit', values);
@@ -56,57 +145,57 @@ function App() {
             <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Year && touched.Year })}
               name='Year'
               validate={validateYear}>
-              <option selected value='-1'>Год выпуска</option>
+              <option selected value=''>Год выпуска</option>
               <option value='2023'>2023</option>
               <option value='2022'>2022</option>
               <option value='2021'>2021</option>
             </Field>
             {errors.Year && touched.Year && <div className={styles.error}>{errors.Year}</div>}
             <br />
-            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Mark && touched.Mark })}
+            <Field disabled as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Mark && touched.Mark })}
               name='Mark'
               validate={validateMark}>
-              <option selected value='-1'>Марка</option>
-              <option value='1'>Audi</option>
-              <option value='2'>BMW</option>
-              <option value='3'>Mercedes-Benz</option>
+              <option selected value=''>Марка</option>
+              <option value='Audi'>Audi</option>
+              <option value='BMW'>BMW</option>
+              <option value='Mercedes-Benz'>Mercedes-Benz</option>
             </Field>
             {errors.Mark && touched.Mark && <div className={styles.error}>{errors.Mark}</div>}
             <br />
-            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Model && touched.Model })}
+            <Field disabled as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Model && touched.Model })}
               name='Model'
               validate={validateModel}>
-              <option selected value='-1'>Модель</option>
+              <option selected value=''>Модель</option>
               <option value='1'>Ывыводллододл одл100</option>
               <option value='2'>BMW</option>
               <option value='3'>Mercedes-Benz</option>
             </Field>
             {errors.Model && touched.Model && <div className={styles.error}>{errors.Model}</div>}
             <br />
-            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Generation && touched.Generation })}
-              name='Model'
+            <Field disabled as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Generation && touched.Generation })}
+              name='Generation'
               validate={validateGeneration}>
-              <option selected value='-1'>Поколение</option>
+              <option selected value=''>Поколение</option>
               <option value='1'>Generation 1</option>
               <option value='2'>Generation 2</option>
               <option value='3'>Generation 3</option>
             </Field>
             {errors.Generation && touched.Generation && <div className={styles.error}>{errors.Generation}</div>}
             <br />
-            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Configuration && touched.Configuration })}
-              name='Model'
+            <Field disabled as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Configuration && touched.Configuration })}
+              name='Configuration'
               validate={validateConfiguration}>
-              <option selected value='-1'>Конфигурация</option>
+              <option selected value=''>Конфигурация</option>
               <option value='1'>Configuration 1</option>
               <option value='2'>Configuration 2</option>
               <option value='3'>Configuration 3</option>
             </Field>
             {errors.Configuration && touched.Configuration && <div className={styles.error}>{errors.Configuration}</div>}
             <br />
-            <Field as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Modification && touched.Modification })}
+            <Field disabled as='select' className={classnames(styles.field, { [styles.errorInput]: errors.Modification && touched.Modification })}
               name='Modification'
               validate={validateModification}>
-              <option selected value='-1'>Модификация</option>
+              <option selected value=''>Модификация</option>
               <option value='1'>Modification 1</option>
               <option value='2'>Modification 2</option>
               <option value='3'>Modification 3</option>
