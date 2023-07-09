08382bb73161ab1748a4e868563356c139bf96ee
viroff <viroff_zz@mail.ru>
2023-07-09 16:21:23 +0300

    fileuploader test

--- src/Components/FileUploader.js
+++ src/Components/FileUploader.js
@@ -9,63 +9,61 @@ const FileUploader = (props) => {
         props.setImages(imageList);
     };
     return (
-        <input type='file' id="files" accept="image/jpg, image/jpeg" multiple></input> 
-        // <div className="Uploader">
-        //     <ImageUploading
-        //         multiple
-        //         value={props.images}
-        //         onChange={onChange}
-        //         acceptType={['png', 'jpg', 'jpeg']}
-        //         maxNumber={maxNumber}
-        //         dataURLKey="data"
-        //     >
-        //         {({
-        //             imageList,
-        //             onImageUpload,
-        //             onImageRemoveAll,
-        //             onImageUpdate,
-        //             onImageRemove,
-        //             isDragging,
-        //             dragProps,
-        //         }) => (
-        //             <>
-        //             {imageList.length}
-        //                 <div className='row-divider' />
-        //                 <div className="centerpanel panel column">
-        //                     {imageList.map((image, index) => (
-        //                         <div key={'dwx' + index} className='imgcontainer'>
-        //                             <Image key={index} src={image['data']} width="290" />
-        //                             <Button
-        //                                 appearance='transparent'
-        //                                 key={'bsd' + index}
-        //                                 icon={<DismissCircle32Filled />}
-        //                                 className='imgbutton'
-        //                                 onClick={() => onImageRemove(index)} />
-        //                         </div>
-        //                     ))}
-        //                 </div>
-        //                 <div className="centerpanel">
-        //                     <Button
-        //                         size="normal"
-        //                         style={isDragging ? { color: 'red' } : undefined}
-        //                         onClick={onImageUpload}
-        //                         {...dragProps}
-        //                         type='button'
-        //                     >
-        //                         Загрузить...
-        //                     </Button>
-        //                     <Button
-        //                         disabled={imageList.length === 0}
-        //                         size="normal"
-        //                         onClick={onImageRemoveAll}
-        //                         type='button'>
-        //                         Очистить
-        //                     </Button>
-        //                 </div>
-        //             </>
-        //         )}
-        //     </ImageUploading>
-        // </div>
+        <div className="Uploader">
+            <ImageUploading
+                value={props.images}
+                onChange={onChange}
+                acceptType={['png', 'jpg', 'jpeg']}
+                maxNumber={maxNumber}
+                dataURLKey="data"
+            >
+                {({
+                    imageList,
+                    onImageUpload,
+                    onImageRemoveAll,
+                    onImageUpdate,
+                    onImageRemove,
+                    isDragging,
+                    dragProps,
+                }) => (
+                    <>
+                    {imageList.length}
+                        <div className='row-divider' />
+                        <div className="centerpanel panel column">
+                            {imageList.map((image, index) => (
+                                <div key={'dwx' + index} className='imgcontainer'>
+                                    <Image key={index} src={image['data']} width="290" />
+                                    <Button
+                                        appearance='transparent'
+                                        key={'bsd' + index}
+                                        icon={<DismissCircle32Filled />}
+                                        className='imgbutton'
+                                        onClick={() => onImageRemove(index)} />
+                                </div>
+                            ))}
+                        </div>
+                        <div className="centerpanel">
+                            <Button
+                                size="normal"
+                                style={isDragging ? { color: 'red' } : undefined}
+                                onClick={onImageUpload}
+                                {...dragProps}
+                                type='button'
+                            >
+                                Загрузить...
+                            </Button>
+                            <Button
+                                disabled={imageList.length === 0}
+                                size="normal"
+                                onClick={onImageRemoveAll}
+                                type='button'>
+                                Очистить
+                            </Button>
+                        </div>
+                    </>
+                )}
+            </ImageUploading>
+        </div>
     );
 };
 
